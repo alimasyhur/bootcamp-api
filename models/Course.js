d5aeb2baad85246b44e81cfcf32ecf1981333481
Ali masyhur <jegrag4ever@gmail.com>
2020-03-16 21:59:24 +0700

    add course model and seeder

---
+++ models/Course.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add a title'],
+        trim: true,
+        maxlength: [50, 'Title can not be more than 50 characters']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description']
+    },
+    weeks: {
+        type: String,
+        required: [true, 'Please add a weeks']
+    },
+    tuition: {
+        type: Number,
+        required: [true, 'Please add a tuition cost']
+    },
+    minimumSkill: {
+        type: String,
+        required: [true, 'Please add a minimum skill'],
+        enum: ['beginner', 'intermediate', 'advanced']
+    },
+    scholarshipAvailable: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    bootcamp: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Bootcamp',
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Course', CourseSchema);

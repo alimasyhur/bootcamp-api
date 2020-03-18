75c3d03c7ef5513752ad1d8a433b33e4220c8037
Ali masyhur <jegrag4ever@gmail.com>
2020-03-18 11:02:47 +0700

    add upload file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const fileupload = require('express-fileupload');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 
@@ -19,6 +21,12 @@ app.use(express.json());
 if(process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
 }
+
+// File upload
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);

d2340a859c8fe1121d5867ca01cce821fb487a9b
Ali masyhur <jegrag4ever@gmail.com>
2020-05-30 21:38:05 +0700

    add average rating function

--- models/Review.js
+++ models/Review.js
@@ -34,6 +34,40 @@ const ReviewSchema = new mongoose.Schema({
     }
 });
 
+
+// Static metho to get avg of course ratings
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Call getAverageRating after save
+ReviewSchema.post('save', function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before remove
+ReviewSchema.pre('remove', function () {
+    this.constructor.getAverageRating(this.bootcamp);
+});
+
 // One user one review on one bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
